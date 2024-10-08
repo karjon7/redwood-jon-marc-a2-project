598ff134fd0808bffe5a2bba227c58626167c912
karjon7 <80655086+karjon7@users.noreply.github.com>
2024-10-08 18:20:41 -0400

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,36 @@
+﻿// Include code libraries you need below (use the namespace).
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace Game10003
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetSize(800, 600);
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            Window.ClearBackground(Color.OffWhite);
+
+            Text.Color = Color.Black;
+            Text.Size = 100;
+            Text.Draw($"Hello World!", 0, 0);
+        }
+    }
+}
